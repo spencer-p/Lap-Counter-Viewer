6175db98fe5f874b8c750def719f0f08c394560f
Spencer Peterson <spencerjpeterson@gmail.com>
2017-08-15 14:38:47 -0700

    initial ticker+leaderboard

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var app = function() {
+
+	var self = {};
+
+    self.initialize = function() {
+		$("#app").show();
+    };
+
+    self.insert = function() {
+        self.vue.ticker.splice(0, 0, Date());
+        if (self.vue.ticker.length > 10) {
+            self.vue.ticker.pop()
+        }
+    }
+
+	self.vue = new Vue({
+		el: "#app",
+		data: {
+            leaderboard: [],
+            ticker: []
+		},
+		methods: {
+            insert: self.insert
+		}
+	});
+
+	return self;
+};
+
+var APP = null;
+
+jQuery(function() {
+	APP = app();
+	APP.initialize();
+});
