8c62112d4792056afc9656f777d77c57745e292a
Spencer Peterson <spencerjpeterson@gmail.com>
2017-08-17 14:01:12 -0700

    lint and add gitignore

--- js/index.js
+++ js/index.js
@@ -2,25 +2,25 @@ var app = function() {
 
 	var self = {};
 
-    self.initialize = function() {
+	self.initialize = function() {
 		$("#app").show();
-    };
+	};
 
-    self.insert = function() {
-        self.vue.ticker.splice(0, 0, Date.now());
-        if (self.vue.ticker.length > 10) {
-            self.vue.ticker.pop()
-        }
-    }
+	self.insert = function() {
+		self.vue.ticker.splice(0, 0, Date.now());
+		if (self.vue.ticker.length > 10) {
+			self.vue.ticker.pop()
+		}
+	}
 
 	self.vue = new Vue({
 		el: "#app",
 		data: {
-            leaderboard: [1, 2, 3],
-            ticker: []
+			leaderboard: [1, 2, 3],
+			ticker: []
 		},
 		methods: {
-            insert: self.insert
+			insert: self.insert
 		}
 	});
 
